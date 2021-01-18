702f782a27ec079914570dc074128fb3ce2c3f41
Jack Labonte <JackLab@live.ca>
2021-01-18 18:11:45 -0500

    ViewController

---
+++ DoIt/Task.swift
@@ -0,0 +1,13 @@
+//
+//  Task.swift
+//  DoIt
+//
+//  Created by Jacques Labonte on 2021-01-18.
+//
+
+import Foundation
+
+class Task{
+    var name = ""
+    var important = false
+}
--- DoIt/ViewController.swift
+++ DoIt/ViewController.swift
@@ -7,14 +7,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tblDoit: UITableView!
+    
+    var tasks: [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("DoIt")
+        
+        tasks = fillTaskArray()
+        
+        tblDoit.dataSource = self
+        tblDoit.delegate = self
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let task = tasks[indexPath.row]
+        
+        if task.important{
+            cell.textLabel?.text = "❗️ \(task.name)"
+        } else {
+            cell.textLabel?.text = task.name
+        }
+        
+        return cell
     }
 
-
+    func fillTaskArray() ->[Task] {
+        
+        let t1 = Task()
+        t1.name = "Get haircut"
+        t1.important = true
+        
+        let t2 = Task()
+        t2.name = "Get grocery"
+        t2.important = false
+        
+        let t3 = Task()
+        t3.name = "Walk the dog"
+        t3.important = true
+        
+        return [t1, t2, t3] 
+    }
+    
+    
+    @IBAction func addPushed(_ sender: Any) {
+        performSegue(withIdentifier: "addSegue", sender: nil)
+    }
 }
 
